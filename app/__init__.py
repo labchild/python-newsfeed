69ea49d053c99ac6233ad9dd12b6cbf9cf562350
l bates childs <labchilds@gmail.com>
2022-09-04 22:21:23 -0400

    add session and db connection to app scripts

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from app.routes import home, dashboard
+from app.db import init_db
 
 def create_app(test_config=None):
     # set up app config
@@ -16,5 +17,8 @@ def create_app(test_config=None):
     # register blueprint/connect routes
     app.register_blueprint(home)
     app.register_blueprint(dashboard)
+
+    # open database connection
+    init_db()
 
     return app
--- app/db/__init__.py
+++ app/db/__init__.py
@@ -10,3 +10,12 @@ load_dotenv()
 engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
+
+# start db connection
+def init_db():
+    Base.metadata.create_all(engine)
+
+# send get session object for use with routes
+def get_db():
+    return Session()
+    
